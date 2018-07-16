db19797da6a1634a261b416e676bd6216ec0477a
Pomeisl Ferenc <pomeisl.ferenc@otpebiz.hu>
2018-07-16 13:45:46 +0200

    more meaningful comment in NeighboursCounter.java

--- src/main/java/pe/accounting/conways/utils/NeighboursCounter.java
+++ src/main/java/pe/accounting/conways/utils/NeighboursCounter.java
@@ -4,6 +4,13 @@ import static pe.accounting.conways.common.Consts.ALIVE;
 
 public final class NeighboursCounter {
 
+	/**
+	 * Return numbers of living neighbours around a specified point.
+	 * @param board
+	 * @param x
+	 * @param y
+	 * @return
+	 */
 	public static int count(int[][] board, int x, int y) {
 
 		if (board == null) {
@@ -14,10 +21,10 @@ public final class NeighboursCounter {
 
 		for (int i = x - 1; i <= x + 1; i++) {
 
-			if (i >= 0 && i < board.length) { // fixed here
+			if (i >= 0 && i < board.length) { // existing rows
 				for (int j = y - 1; j <= y + 1; j++) {
 
-					if (j >= 0 && j < board[i].length) { // fixed here
+					if (j >= 0 && j < board[i].length) { // existing columns
 						if (i != x || j != y) {
 							if (board[i][j] == ALIVE) {
 								count++;
