10a80acf5fc3e1284079436f37eb7d19324b7ae1
Ferenc Pomeisl <ferencpomeisl@gmail.com>
2018-07-17 03:30:27 +0200

    change loop to scheduling

--- src/main/java/pe/accounting/conways/ConwaysApp.java
+++ src/main/java/pe/accounting/conways/ConwaysApp.java
@@ -1,5 +1,10 @@
 package pe.accounting.conways;
 
+import java.util.Timer;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 import pe.accounting.conways.game.GameOfLife;
 import pe.accounting.conways.game.RandomGameOfLifeProxy;
 import pe.accounting.conways.ui.SwingRepresentation;
@@ -14,7 +19,9 @@ public class ConwaysApp {
 		observableGame.subscribe(window);
 
 		Runnable runnableGame = (Runnable) observableGame;
-		runnableGame.run();
+		
+		ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+		scheduler.scheduleAtFixedRate(runnableGame, 0, 10L, TimeUnit.MILLISECONDS);
 
 	}
 
